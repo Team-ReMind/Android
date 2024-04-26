2beb83dee6f13ee249d55c26f77938faf0cac781
seunghee17 <victoria9981@swu.ac.kr>
2024-04-26 16:55:49 +0900

    test commit

--- app/src/main/java/com/example/remind/MainActivity.kt
+++ app/src/main/java/com/example/remind/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Hello World")
+                    Greeting("리마인드 입니당")
                 }
             }
         }

5df0184c9f32770636c5ce390a3f7fa0cc86b535
seunghee17 <victoria9981@swu.ac.kr>
2024-05-14 12:45:25 +0900

    [feat #5] merge오류 해결

--- app/src/main/java/com/example/remind/feature/screens/doctor/DoctorRegisterScreen.kt
+++ app/src/main/java/com/example/remind/feature/screens/doctor/DoctorRegisterScreen.kt
@@ -22,18 +22,23 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.remind.R
+import com.example.remind.core.common.component.BasicBackAppBar
 import com.example.remind.core.common.component.BasicButton
 import com.example.remind.core.designsystem.theme.RemindTheme
 
 @Composable
-fun DoctorRegisterScreen() {
+fun DoctorRegisterScreen(
+    navController: NavHostController,
+) {
     RemindTheme {
         Column(
         ) {
-            TopBar(
+            BasicBackAppBar (
                 modifier = Modifier,
-                onClick = {}
+                onClick = {navController.navigateUp()},
+                title = stringResource(id = R.string.환자_추가하기)
             )
             Column(
                 modifier = Modifier
@@ -51,34 +56,6 @@ fun DoctorRegisterScreen() {
                 )
             }
         }
-    }
-}
-
-@Composable
-fun TopBar(
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color = RemindTheme.colors.white),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            modifier = Modifier
-                .padding(start = 20.dp, top = 13.dp, bottom = 16.dp)
-                .clickable(onClick = onClick),
-            painter = painterResource(id = R.drawable.ic_back),
-            contentDescription = null,
-            tint = RemindTheme.colors.icon
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = stringResource(id = R.string.환자_추가하기),
-            style = RemindTheme.typography.b1Bold.copy(color = RemindTheme.colors.text)
-        )
-        Spacer(modifier = Modifier.weight(1f))
     }
 }
 
@@ -203,5 +180,5 @@ fun RequestListItem(
 @Composable
 fun ScreenPreview() {
     //CodeContainer(modifier = Modifier, code = "1A2BVZ")
-    DoctorRegisterScreen()
+    //DoctorRegisterScreen()
 }

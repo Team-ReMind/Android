7c0502fc2c6a38df74c504f4785ab40b8e2335e9
seunghee17 <victoria9981@swu.ac.kr>
2024-05-17 17:30:56 +0900

    [feat #12] 약 복용정보 조회 api 호출 정의

--- app/src/main/java/com/example/remind/data/network/service/PatientService.kt
+++ app/src/main/java/com/example/remind/data/network/service/PatientService.kt
@@ -1,4 +1,14 @@
 package com.example.remind.data.network.service
 
+import com.example.remind.data.model.response.MedicineInfoResponse
+import com.example.remind.data.network.adapter.ApiResult
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface PatientService {
+    @GET("/taking-medicine")
+    suspend fun getMedicineInfo(
+        @Path("memberId") memberId:Int,
+        @Path("date") string:Int,
+    ): ApiResult<MedicineInfoResponse>
 }
